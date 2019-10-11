1fe6909397d02a9fec9ca759e07075e4869e19d7
SavannahBolger <C00215747@itcarlow.ie>
2019-10-11 09:27:02 +0100

    lab 4 start

---
+++ Lab 4/Lab 4/main.cpp
@@ -0,0 +1,14 @@
+/// <summary>
+/// Entity Component System
+/// @author: Savannah Bolger
+/// @date: 10/10/2019
+/// </summary>
+
+#include <iostream>
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
